0f9f339ab5da168c6f5a9e1194a48ab43e2fc96b
Rigidmax <damilolaajisafe809@gmail.com>
2022-10-13 21:41:44 -0700

    Another commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * @void: Function argument
+ * Return: 0 if the code runs successfuly. 1 otherwise
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
