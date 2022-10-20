097ebc53d5902ccef831994507b3c3a958952533
Rigidmax <damilolaajisafe809@gmail.com>
2022-10-20 08:29:23 -0700

    Another commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -16,3 +16,47 @@ int main(void)
     printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * Description: checks is a character is uppercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+int _isupper(int c)
+{
+int i = A;
+
+for (i = A; i <= Z; i++)
+{
+/* refer int c*/
+if (c == i)
+{
+return (1);
+}
+}
+return (0);
+}
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * Description: checks is a character is uppercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+int _isupper(int c)
+{
+    int i = A;
+
+    for (i = A; i <= Z; i++)
+    {
+    /* refer int c*/
+    if (c == i)
+    {
+    return (1);
+    }
+    }
+    return (0);
+}
