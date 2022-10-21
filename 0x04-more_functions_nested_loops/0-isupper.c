b383cc22b52c4104b9a82cc2a80e235e2569cd3b
Rigidmax <damilolaajisafe809@gmail.com>
2022-10-20 20:13:16 -0700

    Another commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,62 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
  */
-int main(void)
-{
-    char c;
 
-    c = A;
-    printf("%c: %d\n", c, _isupper(c));
-    c = a;
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
-}
-#include "main.h"
-
-/**
- * _isupper - Entry point
- * Description: checks is a character is uppercase
- * @c: the integer value it receives
- * Return: 1 if true. 0 if false.
- */
 int _isupper(int c)
 {
-int i = A;
-
-for (i = A; i <= Z; i++)
-{
-/* refer int c*/
-if (c == i)
-{
-return (1);
-}
-}
-return (0);
-}
-#include "main.h"
-
-/**
- * _isupper - Entry point
- * Description: checks is a character is uppercase
- * @c: the integer value it receives
- * Return: 1 if true. 0 if false.
- */
-int _isupper(int c)
-{
-    int i = A;
-
-    for (i = A; i <= Z; i++)
-    {
-    /* refer int c*/
-    if (c == i)
-    {
-    return (1);
-    }
-    }
-    return (0);
+        if (c >= 65 && c <= 90)
+        {
+                return (1);
+        }
+        return (0);
 }
