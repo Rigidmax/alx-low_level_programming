cc0d25813135f3af9084ac45b5fd6393732b99c9
Rigidmax <damilolaajisafe809@gmail.com>
2022-10-24 14:29:39 -0700

    Another commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * _strlen - returns the len of a str
+ * @s : string
+ * Return: length of string
  */
+
 int _strlen(char *s)
-{
-int longi = 0;
 
-  while (*s != 0)
 {
-longi++;
-s++;
-}
+        int len = 0;
 
-return (longi);
+        while (s[len] != 0)
+        {
+                len++;
+        }
+
+        return (len);
 }
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
+ * _puts - write string followed by new line
+ * @str: string
+ * Return: void
  */
 void _puts(char *str)
 {
-        while (*str != 0)
-        {
+        while (*str)
                 _putchar(*str++);
-        }
+
         _putchar(n);
 }
