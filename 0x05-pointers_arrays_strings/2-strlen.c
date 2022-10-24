ebbe904a044f56f66a0d0b2e3ac12391a0cab774
Rigidmax <damilolaajisafe809@gmail.com>
2022-10-24 14:43:09 -0700

    Another commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,18 @@
 #include "main.h"
-
 /**
- * _strlen - returns the len of a str
- * @s : string
- * Return: length of string
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-
 int _strlen(char *s)
-
 {
-        int len = 0;
+int longi = 0;
 
-        while (s[len] != 0)
-        {
-                len++;
-        }
+while (*s != 0)
+{
+longi++;
+s++;
+}
 
-        return (len);
+return (longi);
 }
