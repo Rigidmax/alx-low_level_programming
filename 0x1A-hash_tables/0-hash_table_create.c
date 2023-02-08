b3cb7ac47090b433ff390130ff70b81f0db60ce5
Rigidmax <damilolaajisafe809@gmail.com>
2023-02-08 06:52:56 -0800

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - create hash table
+ *
+ * Return: NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size);
+{
+    unsigned long i = 0;
+    for (int j = 0; str[j]; j++)
+        i += str[j];
+    hash_table_t *ht;
+
+    return (NULL);
+}
